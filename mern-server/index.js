45c1c8024eeb8779e9c559137416dac2701e7df6
anshika jain <Y226101005@dhsgsu.edu.in>
2026-01-13 01:20:32 +0530

    Update index.js

--- mern-server/index.js
+++ mern-server/index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 app.use(cors({
     origin: [
         "https://shiksha-kendra-7gfe.vercel.app", // Your Main Frontend URL
-        "http://localhost:5173"                    // Keep for local testing
+                          // Keep for local testing
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"]
